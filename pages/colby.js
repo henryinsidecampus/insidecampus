1c07de6efb270a6b8b0e7823f5b135c92f1411b3
henryinsidecampus <henrydoherty88@gmail.com>
2025-05-22 17:47:07 -0400

    Add Colby College page

---
+++ pages/colby.js
@@ -0,0 +1,8 @@
+export default function ColbyPage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Inter, sans-serif' }}>
+      <h1>Colby College Q&A</h1>
+      <p>This is the Colby hub where admitted and prospective students can see real answers from Colby students.</p>
+    </div>
+  );
+}
