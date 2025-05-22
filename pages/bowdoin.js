b692664f2f20d374ef37b1cb9078bf1d66776ad3
henryinsidecampus <henrydoherty88@gmail.com>
2025-05-22 17:54:35 -0400

    Add Bowdoin College page

---
+++ pages/bowdoin.js
@@ -0,0 +1,8 @@
+export default function BowdoinPage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Inter, sans-serif' }}>
+      <h1>Bowdoin College Q&A</h1>
+      <p>This is the Bowdoin hub where admitted and prospective students can see real answers from Bowdoin students.</p>
+    </div>
+  );
+}
