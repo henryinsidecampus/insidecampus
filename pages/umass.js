78dca432808f5408968423097abc734b080e040c
henryinsidecampus <henrydoherty88@gmail.com>
2025-05-22 17:55:03 -0400

    Add UMass Amherst page

---
+++ pages/umass.js
@@ -0,0 +1,8 @@
+export default function UmassPage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Inter, sans-serif' }}>
+      <h1>UMass Amherst Q&A</h1>
+      <p>This is the UMass hub where admitted and prospective students can see real answers from UMass students.</p>
+    </div>
+  );
+}
