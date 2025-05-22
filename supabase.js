28462e07baed2e87244b90d7a9f2df79918bf7f4
henryinsidecampus <henrydoherty88@gmail.com>
2025-05-22 19:15:32 -0400

    Add Supabase credentials

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://<your-project-id>.supabase.co'
-const supabaseKey = '<your-anon-key>'
+const supabaseUrl = 'https://mohwxiahjpnzrcgtdwiv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vaHd4aWFoanBuempjZ3Rkd2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNzEyNjcsImV4cCI6MjAzMTk0NzI2N30.wvX1xy7yL8ZnZCl_k-RsNTctT_AqCdlxQW1eAxfdNq4'
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
